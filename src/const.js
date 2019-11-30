5ea1849e244110b8856449ad1f1a0530c0260d93
Daria <dasha_net96@mail.ru>
2019-11-30 18:17:59 +0300

    Учебный проект: шаблонизируй это

---
+++ src/const.js
@@ -0,0 +1,20 @@
+const Colors = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+const Days = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+const MonthNames = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export {Colors, Days, MonthNames};
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
 import {createTaskTemplate} from './components/task.js';
 import {createSiteMenuTemplate} from './components/site-menu.js';
+import {generateTasks} from './mock/task.js';
+import {generateFilters} from './mock/filter.js';
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -15,17 +19,35 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
+
+const filters = generateFilters();
+render(siteMainElement, createFilterTemplate(filters));
 render(siteMainElement, createBoardTemplate());
 
-const TaskListElement = siteMainElement.querySelector(`.board__tasks`);
-render(TaskListElement, createTaskEditTemplate());
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const tasks = generateTasks(TASK_COUNT);
+render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-new Array(TASK_COUNT)
-  .fill(``)
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+
+tasks
+  .slice(1, showingTasksCount)
   .forEach(
-      () => render(TaskListElement, createTaskTemplate())
+      (task) => render(taskListElement, createTaskTemplate(task))
   );
 
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createLoadMoreButtonTemplate());
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
