3995ec8d16fcaf88d3374b2ba2df37c829667d0c
Дарья <d.belousova@digital-sector.ru>
2019-12-17 10:26:28 +0300

    Создание абстрактного класса, наследование всех компонентов от абстрактного класса.

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import AbstractComponent from "./abstract-component";
 
 const createSortTemplate = () => (
   `<div class="board__filter-list">
@@ -8,20 +8,8 @@ const createSortTemplate = () => (
     </div>`
 );
 
-export default class Sort {
-  constructor() {
-    this._element = null;
-  }
+export default class Sort extends AbstractComponent {
   getTemplate() {
     return createSortTemplate();
-  }
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-  removeElement() {
-    this._element = null;
   }
 }
--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,6 @@
+import AbstractComponent from "./abstract-component";
 import {MONTHS} from '../const.js';
-import {formatTime, createElement} from '../utils.js';
+import {formatTime} from '../utils.js';
 
 const createHashtagsMarkup = (hashtags) =>
   hashtags
@@ -74,21 +75,12 @@ const createTaskTemplate = (task) => {
   );
 };
 
-export default class Task {
+export default class Task extends AbstractComponent {
   constructor(task) {
+    super();
     this._task = task;
-    this._element = null;
   }
   getTemplate() {
     return createTaskTemplate(this._task);
-  }
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-  removeElement() {
-    this._element = null;
   }
 }
